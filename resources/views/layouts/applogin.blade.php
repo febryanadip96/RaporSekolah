92557fe0a9e395c2319c7c75aca7d2b61919411f
febryanadip96 <febryan_dru@yahoo.com>
2017-09-29 10:31:41 +0700

    perbaikan

--- resources/views/layouts/applogin.blade.php
+++ resources/views/layouts/applogin.blade.php
@@ -33,7 +33,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition login-page">
-    
+
     @yield('content')
 
     <!-- jQuery 3 -->
